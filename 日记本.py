896345719ce4e77ded726331a11f16f943b49875
qiuqiu <2235470400@qq.com>
2026-06-07 19:06:02 +0800

    新日记本

--- 日记本.py
+++ 日记本.py
@@ -1,3 +1,7 @@
+from datetime import datetime
 text=input("请输入今天的日记：")
-with open("diary.txt",'a',encoding="utf-8") as f:
+now = datetime.now()
+time = now.strftime("%Y-%m-%d %H:%M:%S")
+with open("diary.txt", "a", encoding="utf-8") as f:
+    f.write(f"{time}\n")
     f.write(text+'\n')
